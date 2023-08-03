da449ca34cb8afede3d2d31a96dcf3284b9676f0
Dollykhan1 <dollykb35@gmail.com>
2023-08-03 19:15:05 -0400

    fresh commit

--- src/main/java/com/generic/Read.java
+++ src/main/java/com/generic/Read.java
@@ -1,7 +1,19 @@
 package com.generic;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Read {
-public static void get() {
+	@BeforeTest
+//	public void setup() {
+//	Response response = RestAssured.get("https://httpbin.org/get");system.out.println(response.prettyprint());
+//System.out.println(response.prettyPrint());
+
+public static void main (String[]args) {
+	//get();
+}
+	}
+	//@Test
+	//public void Status _status _code_200() {
+		
 	
-}
-}
